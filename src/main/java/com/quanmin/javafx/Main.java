8acfc6c0b7585d2b74a76bc085d8e18724a8efc6
wangyongst <wangyongst@gmail.com>
2020-01-15 17:05:33 +0800

    office

--- src/main/java/com/quanmin/javafx/Main.java
+++ src/main/java/com/quanmin/javafx/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/pddUpload.fxml"));
         primaryStage.setTitle("拼多多数据上传");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 550));
         primaryStage.show();
     }
 
